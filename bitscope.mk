50da6df01cb9c862a4bedfc4a2230760048e4260
Jon Mayo <jon@rm-f.net>
2018-04-13 16:32:10 -0700

    z80tab : generates some tables for z80 emulators

--- bitscope.mk
+++ bitscope.mk
@@ -1,3 +1,7 @@
 bitscope.SRCS = bitscope.c ihex.c exebuf.c console.c bitscope-sdl.c
 bitscope.PKGS = sdl2
 TARGETS += bitscope
+
+# generates tables for our emulator
+z80tab.SRCS = z80tab.c
+TARGETS += z80tab
