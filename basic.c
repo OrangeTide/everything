3624e30acf348660d1cc74840172eeb428a36d37
Jon Mayo <jon@rm-f.net>
2018-05-08 16:11:42 -0700

    basic: toy basic interpreter
    aims at ECMA-55 Minimal BASIC. But it is far from that.

---
+++ basic.c
@@ -0,0 +1,450 @@
+/* basic.c : BASIC interpreter - public domain. */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+
+#define TRACE(m, ...) fprintf(stderr, "%s():%d:" m "\n", __func__, __LINE__, ## __VA_ARGS__)
+
+/******************************************************************************/
+/* Exported types */
+/******************************************************************************/
+
+struct basic_env;
+
+/******************************************************************************/
+/* Data types */
+/******************************************************************************/
+
+struct source_line {
+	struct source_line *next; /* linked list */
+	unsigned short num;
+	unsigned short len;
+	char line[];
+};
+
+/* interpreter environment */
+struct basic_env {
+	struct source_line *lines_head;
+	struct source_line *current_line; /* current program position */
+	int error; /* error flag */
+};
+
+/******************************************************************************/
+/* Internal functions - source line data structure */
+/******************************************************************************/
+
+static void
+line_free(struct source_line *sl)
+{
+	free(sl);
+}
+
+static struct source_line *
+line_new(unsigned linenum, const char *line, int len)
+{
+	struct source_line *sl;
+
+	if (len < 0)
+		len = line ? strlen(line) : 0;
+
+	sl = malloc(sizeof(*sl) + len + 1);
+	if (!sl)
+		return NULL;
+
+	sl->num = linenum;
+	sl->next = NULL;
+	sl->len = len;
+	if (line) {
+		memcpy(sl->line, line, len);
+		sl->line[len] = 0;
+	} else {
+		memset(sl->line, 0, len + 1);
+	}
+
+	return sl;
+}
+
+/* find where we might insert an entry.
+ * returned pointer will be where 'num' should be inserted/replaced. */
+static struct source_line **
+line_find_prev(struct source_line **head, unsigned num)
+{
+	struct source_line *cur, **prev;
+
+	for (prev = head, cur = *head; cur != NULL; prev = &cur->next, cur = cur->next) {
+		if (num <= cur->num)
+			return prev;
+	}
+
+	return prev;
+}
+
+static struct source_line *
+line_find(struct source_line **head, unsigned num)
+{
+	struct source_line **prev;
+
+	prev = line_find_prev(head, num);
+	if (!prev || !*prev)
+		return NULL;
+	if ((*prev)->num != num)
+		return NULL;
+	return *prev;
+}
+
+/* insert/replace a line */
+static int
+line_insert(struct source_line **head, struct source_line *sl)
+{
+	struct source_line **prev, *old;
+
+	TRACE("add num=%d line=\"%s\"", sl->num, sl->line);
+	prev = line_find_prev(head, sl->num);
+	if (!prev)
+		return -1;
+
+	/* there are 3 cases to support:
+	 * 1. empty list,
+	 * 2. replace duplicate item on list,
+	 * 3. insert before next highest item on list.
+	 */
+
+	old = *prev;
+	if (!old) {
+		sl->next = NULL;
+		*prev = sl;
+		return 0;
+	}
+
+	if (old->num == sl->num) { /* replace a duplicate line */
+		sl->next = old->next;
+		old->next = NULL;
+		line_free(old);
+		*prev = sl;
+	} else { /* insert before */
+		assert(sl->num < old->num);
+		sl->next = old;
+		*prev = sl;
+	}
+
+	return 0;
+}
+
+/******************************************************************************/
+/* Internal functions - parser */
+/******************************************************************************/
+
+enum keyword { K_PRINT, K_INPUT, K_IF, K_THEN, K_GOTO, K_FOR, K_NEXT, K_GOSUB,
+	K_RETURN, K_END, K_LET, };
+
+static int
+parse_keyword(const char *s, char **endptr)
+{
+	/* Format is: <length><string><length><string> ... NUL
+	 * must be in the same order as enum keyword */
+	const char kinfo[] = "5PRINT5INPUT2IF4THEN4GOTO3FOR4NEXT5GOSUB6RETURN3END3LET";
+	unsigned pos, len;
+	enum keyword keyword;
+
+	for (keyword = 0, pos = 0; kinfo[pos]; pos += len, keyword++) {
+		len = kinfo[pos++] - '0';
+		if (strncasecmp(s, kinfo + pos, len) == 0) {
+			if (endptr)
+				*endptr = (char*)s + len;
+			return keyword;
+		}
+	}
+
+	return -1; /* no keyword match */
+}
+
+/* evalutes numeric expression (floating point) */
+static int
+eval_numeric_expression(const char *s, char **endptr, double *out)
+{
+	char *end;
+	double num;
+
+	// TODO: implement an expression parser (recursive or shunting yard)
+
+	/* get a single number (floating point) */
+	errno = 0;
+	num = strtod(s, &end);
+	if (end == s || errno) {
+		return -1;
+	}
+
+	if (endptr)
+		*endptr = end;
+	if (out)
+		*out = num;
+
+	return 0;
+}
+
+static int
+eval_string_expression(const char *s, char **endptr, char **out)
+{
+	return -1; // TODO: implement this
+}
+
+/* parse and execute a PRINT statement */
+static int
+exec_stmt_print(const char *args)
+{
+	const char *cur;
+	char *end;
+	int result;
+	double num;
+	char *svalue;
+
+	cur = args;
+
+	/* discard optional leading spaces */
+	while (isspace(*cur))
+		cur++;
+
+	while (*cur) {
+		TRACE("running...");
+
+		if (eval_numeric_expression(cur, &end, &num) == 0) {
+			// TODO: print a number
+			printf("%10g", num);
+		} else if (eval_string_expression(cur, &end, &svalue) == 0) {
+			// TODO: print a string
+		} else {
+			return -1; /* failure */
+		}
+
+		cur = end;
+
+		/* discard optional spaces before ; */
+		while (isspace(*cur))
+			cur++;
+
+		if (*cur == ';') {
+			cur++;
+			// TODO: handle ;
+		} else if (*cur == ',') {
+			cur++;
+			// TODO: handle ,
+		} else if (!*cur) {
+			break;
+		}
+
+		/* discard optional trailing spaces */
+		while (isspace(*cur))
+			cur++;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
+
+/* parse and execute an INPUT statement */
+static int
+exec_stmt_input(const char *args)
+{
+	// TODO: parse arguments for INPUT
+	printf("TODO: read input\n");
+
+	return 0;
+}
+
+/******************************************************************************/
+/* Internal functions - interpreter */
+/******************************************************************************/
+
+/* add a null-terminated line to the environment */
+static int
+env_add_line(struct basic_env *env, int linenum, const char *line)
+{
+	struct source_line *sl;
+
+	sl = line_new(linenum, line, -1);
+
+	if (line_insert(&env->lines_head, sl)) {
+		line_free(sl);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int
+env_execute(struct basic_env *env, const char *line)
+{
+	const char *cur;
+	char *end;
+	int result;
+
+	cur = line;
+	/* discard leading spaces */
+	while (isspace(*cur))
+		cur++;
+
+	TRACE("execute line=\"%s\"", line);
+	result = parse_keyword(cur, &end);
+	if (result == -1) {
+		// TODO: check to see if this is really an assignment statement (LET without LET)
+		return -1; /* invalid */
+	}
+
+	switch ((enum keyword)result) {
+	case K_PRINT:
+		return exec_stmt_print(end);
+	case K_INPUT:
+		return exec_stmt_input(end);
+	default:
+		return -1; /* unknown */
+	}
+
+	// TODO: interprete a line
+
+	return 0;
+}
+
+/******************************************************************************/
+/* Exported functions */
+/******************************************************************************/
+
+void
+basic_free(struct basic_env *env)
+{
+	if (!env)
+		return;
+
+	/* free all the lines */
+	while (env->lines_head) {
+		struct source_line *cur = env->lines_head;
+
+		env->lines_head = cur->next;
+		cur->next = NULL;
+		line_free(cur);
+	}
+
+	/* free the outer structure */
+	free(env);
+}
+
+/* allocates a new interpreter environment.
+ * return NULL on failure. */
+struct basic_env *
+basic_new(void)
+{
+	struct basic_env *env;
+
+	env = calloc(1, sizeof(*env));
+
+	return env;
+}
+
+/* parses a line into the environment.
+ * return 0 on success, -1 on failure. */
+int
+basic_line(struct basic_env *env, const char *line)
+{
+	const char *cur;
+	char *end;
+	int linenum;
+
+	cur = line;
+	errno = 0;
+	linenum = strtoul(cur, &end, 10);
+	if (end != cur) {
+		/* add line */
+		while (isspace(*end))
+			end++;
+		cur = end;
+
+		env_add_line(env, linenum, cur);
+	} else {
+		/* interpret immediately */
+
+		env_execute(env, cur);
+	}
+
+	return 0;
+}
+
+/* step through the program a line at a time.
+ * return 1 if there are more lines.
+ * return 0 if there are no more lines.
+ * return -1 on error.
+ */
+int
+basic_step(struct basic_env *env)
+{
+	struct source_line *cur;
+
+	cur = env->current_line;
+
+	/* initialize as a new program if there is no current line */
+	if (!cur) {
+		// TODO: initialize variables
+		cur = env->lines_head;
+	}
+
+	if (cur) {
+		env->current_line = cur->next; /* update current line */
+		if (env_execute(env, cur->line)) {
+			return -1;
+		}
+	}
+
+	return env->current_line ? 1 : 0;
+}
+
+void
+basic_reset(struct basic_env *env)
+{
+	env->current_line = NULL;
+}
+
+int
+basic_run(struct basic_env *env)
+{
+	struct source_line *cur, *next;
+	int result;
+
+	basic_reset(env);
+
+	do {
+		result = basic_step(env);
+	} while (result == 1);
+
+	TRACE("result=%d", result);
+	return result;
+}
+
+/******************************************************************************/
+/* Main */
+/******************************************************************************/
+
+int
+main(int argc, char **argv)
+{
+	struct basic_env *env;
+
+	env = basic_new();
+	if (!env)
+		return 1; /* error */
+	// simplistic program
+	basic_line(env, "10 PRINT 1.2345; -3.21; +5e5");
+	// more advanced program
+	// basic_line(env, "20 PRINT \"What is your name?\"");
+	// basic_line(env, "30 INPUT A$");
+	basic_line(env, "LIST");
+	if (basic_run(env)) {
+		TRACE("error"); // TBD
+		return 1; /* error */
+	}
+	basic_free(env);
+
+	return 0;
+}
