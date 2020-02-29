46620d77d8cf1b6ddf24dcd2967e6b4c023377e5
Jon Mayo <jon@rm-f.net>
2020-02-28 23:36:30 -0800

    remove obsolete stuff

--- console.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSOLE_H
-#define CONSOLE_H
-#include <stddef.h>
-int console_init(void);
-int console_run(void);
-void console_shutdown(void);
-void console_out(const void *b, size_t n);
-int console_getch(void);
-#endif
--- ihex.h
+++
@@ -1,5 +0,0 @@
-/* ihex.h : Intel HEX parser - public domain */
-#ifndef IHEX_H
-#define IHEX_H
-int ihex_load(const char *filename, int (*process)(unsigned address, unsigned count, unsigned char *data));
-#endif
